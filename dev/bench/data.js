5cec2c8a3fd9db63ea296aa17099453ce057d69f
github-action-benchmark <github@users.noreply.github.com>
2022-09-26 05:30:38 +0000

    add Rust Benchmark (cargo) benchmark result for a36819f17aeaf24b30a5f192b93519500a11b868

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1664170238853,
+  "repoUrl": "https://github.com/speedy-js/rspack",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "IWANABETHATGUY@qq.com",
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "committer": {
+            "email": "IWANABETHATGUY@qq.com",
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "distinct": true,
+          "id": "a36819f17aeaf24b30a5f192b93519500a11b868",
+          "message": "chore: 🤖 change to PR_STATS_COMMENT_TOKEN",
+          "timestamp": "2022-09-26T13:21:24+08:00",
+          "tree_id": "b185296c24f60e73f973469a5cbacb94829ec68a",
+          "url": "https://github.com/speedy-js/rspack/commit/a36819f17aeaf24b30a5f192b93519500a11b868"
+        },
+        "date": 1664170236347,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "lodash",
+            "value": 63121179,
+            "range": "± 1768059",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "css_heavy",
+            "value": 34332340,
+            "range": "± 335177",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
