75fe256173b9e6e7a07d5d53ae61b8fc3698e6a8
github-action-benchmark <github@users.noreply.github.com>
2023-02-06 23:07:08 +0800

    add Rust Benchmark (cargo) benchmark result for 29bd27642505fcdc250ad1de47e723c94f38dd2e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675690013589,
+  "lastUpdate": 1675696028446,
   "repoUrl": "https://github.com/modern-js-dev/rspack",
   "entries": {
     "Rust Benchmark": [
@@ -25871,6 +25871,54 @@ window.BENCHMARK_DATA = {
             "name": "high_cost_benchmark/ten_copy_of_threejs_production",
             "value": 4541574880,
             "range": "± 13665732",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yangjianzju@gmail.com",
+            "name": "hardfist",
+            "username": "hardfist"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "29bd27642505fcdc250ad1de47e723c94f38dd2e",
+          "message": "chore: modify LICENSE (#1763)",
+          "timestamp": "2023-02-06T21:10:36+08:00",
+          "tree_id": "e21e3442715dc2bb522595cc6941e00dcf310845",
+          "url": "https://github.com/modern-js-dev/rspack/commit/29bd27642505fcdc250ad1de47e723c94f38dd2e"
+        },
+        "date": 1675696012176,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "criterion_benchmark/css_heavy",
+            "value": 45530209,
+            "range": "± 2411598",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "criterion_benchmark/ten_copy_of_threejs",
+            "value": 532047031,
+            "range": "± 5714399",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "criterion_benchmark/lodash",
+            "value": 69145355,
+            "range": "± 897358",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "high_cost_benchmark/ten_copy_of_threejs_production",
+            "value": 4543047911,
+            "range": "± 14179840",
             "unit": "ns/iter"
           }
         ]
