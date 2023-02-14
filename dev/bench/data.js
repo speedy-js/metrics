1c467e7c391f815380ee52870f1a8583113a8909
github-action-benchmark <github@users.noreply.github.com>
2023-02-14 16:27:52 +0800

    add Rust Benchmark (cargo) benchmark result for f7320e807d65f647e3406d6fccf6f196c36b266c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676362425339,
+  "lastUpdate": 1676363272070,
   "repoUrl": "https://github.com/modern-js-dev/rspack",
   "entries": {
     "Rust Benchmark": [
@@ -28511,6 +28511,54 @@ window.BENCHMARK_DATA = {
             "name": "high_cost_benchmark/ten_copy_of_threejs_production",
             "value": 4522057440,
             "range": "± 14657547",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yangjianzju@gmail.com",
+            "name": "hardfist",
+            "username": "hardfist"
+          },
+          "committer": {
+            "email": "yangjianzju@gmail.com",
+            "name": "hardfist",
+            "username": "hardfist"
+          },
+          "distinct": true,
+          "id": "f7320e807d65f647e3406d6fccf6f196c36b266c",
+          "message": "Revert \"feat: introduce `DevFriendlySplitChunksPlugin`\"\n\nThis reverts commit cbd20f92586e196ce7630b201203e49a37180b7c.",
+          "timestamp": "2023-02-14T16:04:23+08:00",
+          "tree_id": "f44371ae669591725c24398438d6a9cff84c8166",
+          "url": "https://github.com/modern-js-dev/rspack/commit/f7320e807d65f647e3406d6fccf6f196c36b266c"
+        },
+        "date": 1676363267613,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "criterion_benchmark/css_heavy",
+            "value": 43963965,
+            "range": "± 2375189",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "criterion_benchmark/ten_copy_of_threejs",
+            "value": 529624911,
+            "range": "± 5110654",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "criterion_benchmark/lodash",
+            "value": 67598646,
+            "range": "± 946968",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "high_cost_benchmark/ten_copy_of_threejs_production",
+            "value": 4584252710,
+            "range": "± 13708848",
             "unit": "ns/iter"
           }
         ]
