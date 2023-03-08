0dfe1bf75c4b483420a4a5aa503c12b83dfe6629
github-action-benchmark <github@users.noreply.github.com>
2023-03-08 17:13:07 +0800

    add Rust Benchmark (cargo) benchmark result for 8601b067b8b5526fa5152c052c646e275e280efa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678260905447,
+  "lastUpdate": 1678266787142,
   "repoUrl": "https://github.com/web-infra-dev/rspack",
   "entries": {
     "Rust Benchmark": [
@@ -6365,6 +6365,42 @@ window.BENCHMARK_DATA = {
             "name": "high_cost_benchmark/ten_copy_of_threejs_production",
             "value": 4141209728,
             "range": "± 26137085",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yangjian.fe@bytedance.com",
+            "name": "yangjian.fe",
+            "username": "hardfist"
+          },
+          "committer": {
+            "email": "yangjianzju@gmail.com",
+            "name": "hardfist",
+            "username": "hardfist"
+          },
+          "distinct": true,
+          "id": "8601b067b8b5526fa5152c052c646e275e280efa",
+          "message": "chore: fix react examples",
+          "timestamp": "2023-03-08T17:02:32+08:00",
+          "tree_id": "684edfc9aff5de81da462be1d034e0f50166d662",
+          "url": "https://github.com/web-infra-dev/rspack/commit/8601b067b8b5526fa5152c052c646e275e280efa"
+        },
+        "date": 1678266784477,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "criterion_benchmark/ten_copy_of_threejs",
+            "value": 289110735,
+            "range": "± 4982535",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "high_cost_benchmark/ten_copy_of_threejs_production",
+            "value": 4126769475,
+            "range": "± 27944989",
             "unit": "ns/iter"
           }
         ]
