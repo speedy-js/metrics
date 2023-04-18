d677e1167061fe9f4ba72d92140330f78c5ef993
github-action-benchmark <github@users.noreply.github.com>
2023-04-18 21:31:38 +0800

    add Rust Benchmark (cargo) benchmark result for 89bdfedaecffaf15ead392f7fd33f0f6884622da

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681823956176,
+  "lastUpdate": 1681824698197,
   "repoUrl": "https://github.com/web-infra-dev/rspack",
   "entries": {
     "Rust Benchmark": [
@@ -15185,6 +15185,42 @@ window.BENCHMARK_DATA = {
             "name": "high_cost_benchmark/ten_copy_of_threejs_production",
             "value": 4224825795,
             "range": "± 40211758",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "iwanabethatguy@qq.com",
+            "name": "IWANABETHATGUY",
+            "username": "IWANABETHATGUY"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "89bdfedaecffaf15ead392f7fd33f0f6884622da",
+          "message": "fix: 🐛 fix ci a is undefined (#2810)",
+          "timestamp": "2023-04-18T13:09:10Z",
+          "tree_id": "e0fbcce26c3e3c671f68ec702775300a4ee8d6e8",
+          "url": "https://github.com/web-infra-dev/rspack/commit/89bdfedaecffaf15ead392f7fd33f0f6884622da"
+        },
+        "date": 1681824692559,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "criterion_benchmark/ten_copy_of_threejs",
+            "value": 333495929,
+            "range": "± 6261646",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "high_cost_benchmark/ten_copy_of_threejs_production",
+            "value": 4235172369,
+            "range": "± 34403044",
             "unit": "ns/iter"
           }
         ]
