cc71a9629d83ca6e955a05f42eece785f5ee3eb9
github-action-benchmark <github@users.noreply.github.com>
2023-04-23 18:22:21 +0800

    add Rust Benchmark (cargo) benchmark result for 043b9ffa14b123d1bb4733982ec5344075c85c78

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682244030631,
+  "lastUpdate": 1682245341970,
   "repoUrl": "https://github.com/web-infra-dev/rspack",
   "entries": {
     "Rust Benchmark": [
@@ -16589,6 +16589,42 @@ window.BENCHMARK_DATA = {
             "name": "high_cost_benchmark/ten_copy_of_threejs_production",
             "value": 3930314299,
             "range": "± 20527104",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "i.heyunfei@gmail.com",
+            "name": "HeYunfei",
+            "username": "hyf0"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "043b9ffa14b123d1bb4733982ec5344075c85c78",
+          "message": "chore: should exit if the exitCode is not zero at `check_changeset.js` (#2880)",
+          "timestamp": "2023-04-23T07:19:42Z",
+          "tree_id": "1d0c626d333d39414b16a5a78ba83325b1cee552",
+          "url": "https://github.com/web-infra-dev/rspack/commit/043b9ffa14b123d1bb4733982ec5344075c85c78"
+        },
+        "date": 1682245337857,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "criterion_benchmark/ten_copy_of_threejs",
+            "value": 336675565,
+            "range": "± 4483225",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "high_cost_benchmark/ten_copy_of_threejs_production",
+            "value": 4122008966,
+            "range": "± 30304191",
             "unit": "ns/iter"
           }
         ]
