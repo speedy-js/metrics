3408cac46c015d46f96719f9ab8cd7aa0a0c8bca
github-action-benchmark <github@users.noreply.github.com>
2023-06-08 18:17:25 +0800

    add Rust Benchmark (cargo) benchmark result for 558f6fc1cfc5bba53c91e4ffcbfd47c36022b107

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686218268876,
+  "lastUpdate": 1686219445728,
   "repoUrl": "https://github.com/web-infra-dev/rspack",
   "entries": {
     "Rust Benchmark": [
@@ -27425,6 +27425,42 @@ window.BENCHMARK_DATA = {
             "name": "high_cost_benchmark/ten_copy_of_threejs_production",
             "value": 4280405943,
             "range": "± 39789884",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "9aoyuao@gmail.com",
+            "name": "gaoyuan",
+            "username": "9aoy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "558f6fc1cfc5bba53c91e4ffcbfd47c36022b107",
+          "message": "chore(devServer): use webpack-dev-middleware instead of @rspack/dev-middleware (#3478)\n\n* chore: deprecated @rspack/dev-middleware\n\n* chore: update deprecate tip",
+          "timestamp": "2023-06-08T09:45:28Z",
+          "tree_id": "0aa506d175d584cc5534688d20f23040901632fe",
+          "url": "https://github.com/web-infra-dev/rspack/commit/558f6fc1cfc5bba53c91e4ffcbfd47c36022b107"
+        },
+        "date": 1686219442939,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "criterion_benchmark/ten_copy_of_threejs",
+            "value": 395634607,
+            "range": "± 5508332",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "high_cost_benchmark/ten_copy_of_threejs_production",
+            "value": 4422941567,
+            "range": "± 58573814",
             "unit": "ns/iter"
           }
         ]
