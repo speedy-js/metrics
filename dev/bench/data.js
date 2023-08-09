4d27ceac35ce8e592e75028f6b0cb6e247b806bc
github-action-benchmark <github@users.noreply.github.com>
2023-08-09 19:40:47 +0800

    add Rust Benchmark (cargo) benchmark result for 7d23377c514ec6da075889bbf07a9f7348b571f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691574165018,
+  "lastUpdate": 1691581247571,
   "repoUrl": "https://github.com/web-infra-dev/rspack",
   "entries": {
     "Rust Benchmark": [
@@ -35021,6 +35021,42 @@ window.BENCHMARK_DATA = {
             "name": "high_cost_benchmark/ten_copy_of_threejs_production",
             "value": 4902961232,
             "range": "± 40630654",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ahabhgk@gmail.com",
+            "name": "Gengkun",
+            "username": "ahabhgk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7d23377c514ec6da075889bbf07a9f7348b571f7",
+          "message": "feat: support configuration.profile (#3924)\n\n* feat: support configuration.profile\n\n* update etst\n\n* fix test",
+          "timestamp": "2023-08-09T10:51:33Z",
+          "tree_id": "ae03b8a453c3ab2b27623f99aa14671068ef19aa",
+          "url": "https://github.com/web-infra-dev/rspack/commit/7d23377c514ec6da075889bbf07a9f7348b571f7"
+        },
+        "date": 1691581244649,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "criterion_benchmark/ten_copy_of_threejs",
+            "value": 334133850,
+            "range": "± 5748108",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "high_cost_benchmark/ten_copy_of_threejs_production",
+            "value": 4918526145,
+            "range": "± 35462431",
             "unit": "ns/iter"
           }
         ]
